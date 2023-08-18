ea9bac10ea1f8110b089799446a72c0b95c4c76b
Taeyong Lee <d104601@hotmail.com>
2023-08-18 18:27:53 -0400

    changed detail of Pagination features

--- src/components/paginationBar.tsx
+++ src/components/paginationBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 interface PaginationBarProps {
     CurrentOffset: number;
     setCurrentOffset: (offset: number) => void;
@@ -6,21 +7,20 @@ interface PaginationBarProps {
 
 const PaginationBar = (props: PaginationBarProps) => {
     const {CurrentOffset, setCurrentOffset, TotalResults} = props;
+    const [currentPage, setCurrentPage] = useState(CurrentOffset / 10);
+    const [targetPage, setTargetPage] = useState(currentPage + 1);
+    let maxPage = Math.floor(TotalResults / 10);
 
-    let maxPage = TotalResults < 100 ? Math.floor(TotalResults / 10) : 10;
 
-    let pageNumbers = [];
-
-    for (let i = 0; i < maxPage; i++) {
-        pageNumbers.push(i);
-    }
-
+    // page input with current page
     return (
         <nav className="pagination is-centered" role="navigation" aria-label="pagination">
             <button
                 className="pagination-previous"
                 onClick={() => {
                     setCurrentOffset(CurrentOffset - 10);
+                    setCurrentPage(currentPage - 1);
+                    setTargetPage(currentPage);
                 }}
                 disabled={CurrentOffset === 0}
             >
@@ -28,27 +28,55 @@ const PaginationBar = (props: PaginationBarProps) => {
             </button>
 
             <ul className="pagination-list">
-                {pageNumbers.map((pageNumber) => {
-                    return (
-                        <li key={pageNumber}>
-                            <button
-                                className={"pagination-link" + (CurrentOffset === pageNumber * 10 ? " is-current" : "")}
-                                aria-label={"Goto page " + pageNumber}
-                                onClick={() => {
-                                    setCurrentOffset(pageNumber * 10);
-                                }}
-                            >
-                                {pageNumber + 1}
-                            </button>
-                        </li>
-                    )
-                })}
+                <li>
+                    <button
+                        className="pagination-link"
+                        onClick={() => {
+                            setCurrentOffset(0);
+                            setCurrentPage(0);
+                            setTargetPage(1);
+                        }
+                        }
+                        disabled={currentPage === 0}
+                    >
+                        First page
+                    </button>
+                </li>
+                <li>
+                    <input
+                        className="pagination-link"
+                        type="number"
+                        min={1}
+                        max={maxPage}
+                        value={targetPage}
+                        onChange={(e) => {
+                            setTargetPage(Number(e.target.value));
+                        }}
+                        >
+                    </input>
+                </li>
+                <li>
+                    <button
+                        className="pagination-link"
+                        onClick={() => {
+                            setCurrentOffset((targetPage-1) * 10);
+                            setCurrentPage(targetPage-1);
+                            setTargetPage(targetPage);
+                        }
+                        }
+                        disabled={targetPage === currentPage + 1 || targetPage > maxPage}
+                    >
+                        Move
+                    </button>
+                </li>
             </ul>
 
             <button
                 className="pagination-next"
                 onClick={() => {
                     setCurrentOffset(CurrentOffset + 10);
+                    setCurrentPage(currentPage + 1);
+                    setTargetPage(currentPage + 2);
                 }}
                 disabled={CurrentOffset === maxPage * 10}
             >
