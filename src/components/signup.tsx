18a8a502a4aab8600495879329c896f1cedb8f75
Steve Lee <d104601@hotmail.com>
2023-08-20 18:21:12 -0400

    working on signup and signin feature

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -1,10 +1,65 @@
+import {useState} from 'react';
+import { UserService } from '../services/userService';
+
 const Signup = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassowrd] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const [error, setError] = useState('');
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        // check username
+        if (username.length < 3) {
+            setError('Username must be at least 5 characters long');
+            return;
+        }
+        // check email
+        else if (!email.includes('@')) {
+            setError('Invalid email');
+            return;
+        }
+        // check password
+        else if (password.length < 6) {
+            setError('Password must be at least 6 characters long');
+            return;
+        }
+        // check confirm password
+        else if (password !== confirmPassword) {
+            setError('Passwords do not match');
+            return;
+        }
+        else
+        {
+            // if all checks pass, send data to server
+            UserService.signup(username, password, email)
+            .then((response) => {
+                window.location.href = '/login';
+            }).catch((error) => {
+                setError(error.message);
+            });
+        }
+            
+    };
+
     return (
         <div>
             <section className='section'>
                 <div className='box '>
                     <h1 className='title has-text-centered'>Sign Up</h1>
-                    <form>
+                    <form onSubmit={handleSubmit}>
+                        {
+                            error !== ''
+                            &&
+                            <div className='field'>
+                                <div className='notification is-danger'>
+                                    {error}
+                                </div>
+                            </div>
+                        }
                         <div className='field'>
                             <label className='label'>Username</label>
                             <div className='control'>
@@ -12,6 +67,8 @@ const Signup = () => {
                                     className='input'
                                     type='text'
                                     placeholder='Username'
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
                                 />
                             </div>
                         </div>
@@ -21,7 +78,9 @@ const Signup = () => {
                                 <input
                                     className='input'
                                     type='email'
-                                    placeholder='Username'
+                                    placeholder='Email'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
                                 />
                             </div>
                         </div>
@@ -32,6 +91,8 @@ const Signup = () => {
                                     className='input'
                                     type='password'
                                     placeholder='Password'
+                                    value={password}
+                                    onChange={(e) => setPassowrd(e.target.value)}
                                 />
                             </div>
                         </div>
@@ -41,7 +102,9 @@ const Signup = () => {
                                 <input
                                     className='input'
                                     type='password'
-                                    placeholder='Password'
+                                    placeholder='Confirm Password'
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
                                 />
                             </div>
                         </div>
