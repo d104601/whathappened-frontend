71ce6519df04eee57c13e5ed69de14a623294260
Steve Lee <d104601@hotmail.com>
2023-08-20 22:21:25 -0400

    working on login features

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,18 @@
-class userService {}
+import axios from "axios";
+
+class userService {
+    signup = (username: string, password: string, email: string) => {
+        return axios.post(process.env.REACT_APP_SERVER_URL + "/api/user/register", 
+            {
+                username: username,
+                password: password,
+                email: email
+            }
+        ).then((res) => {
+            return res.data;
+        }
+        );
+    }
+}
 
 export const UserService = new userService();
